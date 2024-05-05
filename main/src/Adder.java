ed74dc1d5ac583a51c337c52b00332d756d4a63d
Mykhaylo Korotkov <s31045@pjwstk.edu.pl>
2024-05-05 20:46:20 +0200

    Zadanie 4

--- main/src/Adder.java
+++ main/src/Adder.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Adder {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class Adder {
+
+    public int add(int e, int r){
+        return e+r;
+    }
+
+    public BigDecimal add(ArrayList<BigDecimal> lista){
+        BigDecimal wynik = BigDecimal.ZERO;
+
+        for(int i = 0; i < lista.size(); i++){
+            wynik = wynik.add(lista.get(i));
+        }
+        return wynik;
+    }
+
 }
--- main/src/Multiplier.java
+++ main/src/Multiplier.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Multiplier {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class Multiplier {
+
+    public int Multiply(int x, int y){
+        return x*y;
+    }
+
+    public int Multiply(int x, int y, int w, int q) {
+        return x*y*w*q;
+    }
+
+    public BigDecimal Multiply(BigDecimal a, BigDecimal b){
+        return a.multiply(b);
+    }
+
+    public int Multiply(ArrayList<Integer> lista) {
+        int wynik = 1;
+        for (int i = 0; i < lista.size(); i++) {
+            wynik *= lista.get(i);
+        }
+        return wynik;
+    }
 }
